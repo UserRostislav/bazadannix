2c2c5af0acb36c6ac3acb8f3f9b2811ba748a060
prepod <prepod@6-DELL-AUD408>
2022-10-25 14:04:06 +0300

    Добавьте файлы проекта.

---
+++ bazadannix/Model/DataWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bazadannix.Model
+{
+    public static class DataWorker
+    {
+        // создать отдел
+        // создать позицию
+        // создать сотрудника
+
+        // удалить отдел
+        // удалить позицию
+        // удалить сотрудника
+
+        // редактировать отдел
+        // редактировать позицию
+        // редактировать сотрудника
+
+    }
+}
